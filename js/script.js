55d1afbf1baa46d634df25ceb99a23554a3729e6
imi <kimevangelista40@gmail.com>
2025-04-12 10:47:21 +0800

    update-ui-teacher-login
    update version nung ui sa teacher login

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ togglePassword.addEventListener("click", () => {
   passwordInput.type = isPassword ? "text" : "password";
   togglePassword.textContent = isPassword ? "Show" : "Hide";
 });
+
+//js for bg-changes-start
+const backgroundImages = [
+  '../assets/libertys-path-bg-design-1.png',
+  '../assets/libertys-path-bg-design-2.png',
+  '../assets/libertys-path-bg-design-3.png'
+];
+
+let currentIndex = 0;
+
+function changeBackground() {
+  document.body.style.backgroundImage = `url('${backgroundImages[currentIndex]}')`;
+  currentIndex = (currentIndex + 1) % backgroundImages.length;
+}
+
+changeBackground();
+setInterval(changeBackground, 5000);
+//js for bg-changes-end
